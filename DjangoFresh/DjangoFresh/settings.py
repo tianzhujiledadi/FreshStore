b927266c4b7c7b3b821c75f1ab818e61bba92c78
li <li@qq.com>
2019-07-22 11:46:13 +0800

    建立数据库，收集静态文件

--- DjangoFresh/DjangoFresh/settings.py
+++ DjangoFresh/DjangoFresh/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'DjangoFresh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,  'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,5 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATICFILES_DIRS=(
+#     os.path.join(BASE_DIR,"static"),
+# )
+# MEDIA_URL="/media/"#长传文件
+# MEDIA_ROOT=os.path.join(BASE_DIR,"static")
+STATIC_ROOT=os.path.join(BASE_DIR,"static")
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 生鲜电商网站Django
 
 2019/7/22 DjangoFreshStore开始
+
+同步数据库，收集静态文件。
